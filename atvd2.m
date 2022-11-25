b63b23220d18f61f0283ad61f1a317e096774a7c
Thalles Campagnani <thallescampagnani@gmail.com>
2022-11-25 00:13:00 -0300

    Update atvd2.m

--- atvd2.m
+++ atvd2.m
@@ -4,8 +4,12 @@ clc
 
           %ftemperatura(r,Tc,Te,k,Q,pontos)
           
-[A,B,T] = ftemperatura(0.458,1900,500,0.024,430,15)
+[T1,R1] = ftemperatura(0.458,1900,500,0.024,430,20);
+plot(R1,T1)
+hold on
 
-[A,B,T] = ftemperatura(0.467-0.458,500,360,0.020,0,8)
+[T2,R2] = ftemperatura(0.467-0.458,500,360,0.020,0,20);
+plot(R2+0.458,T2)
 
-[A,B,T] = ftemperatura(0.539-0.467,360,300,0.1070,0,8)
+[T3,R3] = ftemperatura(0.539-0.467,360,300,0.1070,0,20);
+plot(R3+0.467,T3)
